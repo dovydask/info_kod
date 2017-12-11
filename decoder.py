3d3fcfd56efdaa1ff2b74f6b4a55f594cba80adb
DovileP <dovilepreidyte@gmail.com>
2017-12-11 20:43:58 +0200

    Add files via upload

--- decoder.py
+++ decoder.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
+import struct
+import time
+import numpy
+#from bitarray import bitarray
+start_time = time.time()
 
 if len(sys.argv) < 3:
 	print("Trūksta argumentų.")
@@ -11,102 +16,198 @@ elif len(sys.argv) > 3:
 	print("Programos naudojimo pavyzdys:")
 	print("decoder.py [užkoduoto failo vardas] [atkoduoto failo vardas]")
 	sys.exit()
+	
+def c2_decode(input, output):
+	indices = []
+	S = []
+	for s in range(0, 256):
+		S.append(format(s, "08b"))
+		indices.append(s)
 
-def c2_decode(binary_string):
-	while True:
-		zeros = 0
-		start = 0
-		for i in range(start, len(binary_string)):
-			if binary_string[i] == "0":
-				zeros += 1
-			else:
-				break
-		bits_to_get = ""	
-		bits_to_get = binary_string[zeros: (zeros*2)+1]
-		if bits_to_get == "":
-			return values
-			break
-		to_dec = int(bits_to_get, 2)
-		to_decode = binary_string[(zeros*2)+1: (zeros*2+1)+to_dec]
-		decoded_dec = int(to_decode, 2)
-		values.append(decoded_dec-1)
-		binary_string = binary_string[zeros*2+1+to_dec:]
+	locations = dict(zip(indices, S))
+	current_location = 256
 
-def c1_decode(binary_string):
-	while True:
-		zeros = 0
-		start = 0
-		for i in range(start, len(binary_string)):
-			if binary_string[i] == "0":
-				zeros += 1
-			else:
-				break
-		bits_to_get = ""	
-		bits_to_get = binary_string[zeros: (zeros*2)+1]
-		if bits_to_get == "":
-			return values
-			break
-		to_dec = int(bits_to_get, 2)
-		values.append(to_dec-1)
-		binary_string = binary_string[zeros*2+1:]
+	with open(input, "rb") as rf:
+		rf.read(1)
+		byte = rf.read(1)
+		print (byte)
+		binary_string = bin(ord(byte))[2:].rjust(8, '0')
+		#a = bitarray(endian='big')
+		#a.frombytes(byte)
+		#print (a)
+		with open(output, "wb") as wf:
+			while True:
+				zeros = 0
+				start = 0
+				#print(binary_string)
+				i = 0
+				to_break = False
+				while True:
+					if not "1" in binary_string:
+						byte = rf.read(1)
+						if not byte:
+							to_break = True
+							break
+						#print("+", bin(ord(byte))[2:].rjust(8, '0'))
+						binary_string += bin(ord(byte))[2:].rjust(8, '0')
+					if binary_string[i] == "0":
+						zeros += 1
+						i += 1
+					else:
+						binary_string = binary_string[zeros:]
+						#print (binary_string)
+						break
+						
+				if to_break: 
+					break
+					
+				while len(binary_string) <= (zeros + 1):
+					byte = rf.read(1)
+					if not byte:
+						to_break = True
+						break
+					#print("+", bin(ord(byte))[2:].rjust(8, '0'))
+					binary_string += bin(ord(byte))[2:].rjust(8, '0')
+				
+				if to_break:
+					break
+				
+				#print(binary_string, zeros+1)
+				bits_to_get = ""	
+				bits_to_get = binary_string[:zeros+1]
+				#print (bits_to_get)
+				#print("btg:", bits_to_get)
+				if bits_to_get == "":
+					break
+				to_dec = int(bits_to_get, 2)
+				
+				binary_string = binary_string[len(bits_to_get):]
+				#print (binary_string)
+				
+				while len(binary_string) <= (to_dec):
+					byte = rf.read(1)
+					if not byte:
+						to_break = True
+						break
+					#print("+", bin(ord(byte))[2:].rjust(8, '0'))
+					binary_string += bin(ord(byte))[2:].rjust(8, '0')
+				
+				if to_break:
+					break
+				
+				to_decode = binary_string[:to_dec]
+				decoded_dec = int(to_decode, 2)
+				binary_string = binary_string[to_dec:]
+				
+				k = decoded_dec
+				byte_index = current_location - k
+				byte = locations[byte_index]
+				locations = {v: k for k, v in locations.items()}
+				locations[byte] = current_location
+				locations = {v: k for k, v in locations.items()}
+				current_location += 1
+				
+				n = int(byte, 2)
+				data = bytes([n])
+				wf.write(data)
+		
 
+def c1_decode(input, output):
+	indices = []
+	S = []
+	for s in range(0, 256):
+		S.append(format(s, "08b"))
+		indices.append(s)
 
+	locations = dict(zip(indices, S))
+	current_location = 256
 
-indices = []
-S = []
-for s in range(0, 256):
-	S.append(format(s, "08b"))
-	indices.append(s)
+	with open(input, "rb") as rf:
+		rf.read(1)
+		byte = rf.read(1)
+		binary_string = bin(ord(byte))[2:].rjust(8, '0')
+		#a = bitarray(endian='big')
+		#a.frombytes(byte)
+		#print (a)
+		with open(output, "wb") as wf:
+			while True:
+				zeros = 0
+				start = 0
+				#print(binary_string)
+				i = 0
+				to_break = False
+				while True:
+					if not "1" in binary_string:
+						byte = rf.read(1)
+						if not byte:
+							to_break = True
+							break
+						#print("+", bin(ord(byte))[2:].rjust(8, '0'))
+						binary_string += bin(ord(byte))[2:].rjust(8, '0')
+					if binary_string[i] == "0":
+						zeros += 1
+						i += 1
+					else:
+						binary_string = binary_string[zeros:]
+						break
+						
+				if to_break: 
+					break
+					
+				while len(binary_string) <= (zeros + 1):
+					byte = rf.read(1)
+					if not byte:
+						to_break = True
+						break
+					#print("+", bin(ord(byte))[2:].rjust(8, '0'))
+					binary_string += bin(ord(byte))[2:].rjust(8, '0')
+				
+				if to_break:
+					break
+				
+				#print(binary_string, zeros+1)
+				bits_to_get = ""	
+				bits_to_get = binary_string[:zeros+1]
+				#print("btg:", bits_to_get)
+				if bits_to_get == "":
+					break
+				to_dec = int(bits_to_get, 2)
 
-locations = dict(zip(indices, S))
-current_location = 256
-
+				binary_string = binary_string[zeros+1:]
+				
+				k = to_dec
+				byte_index = current_location - k
+				byte = locations[byte_index]
+				locations = {v: k for k, v in locations.items()}
+				locations[byte] = current_location
+				locations = {v: k for k, v in locations.items()}
+				current_location += 1
+				
+				n = int(byte, 2)
+				data = bytes([n])
+				wf.write(data)
+				
+				
 uzkoduota = sys.argv[1]
 rezultatas = sys.argv[2]
-
-values = []
 c2_rezimas = False
+
 
 with open(uzkoduota, "rb") as f:
 	baitai = []
+	
 	mode = f.read(1)
 	mode = bin(ord(mode))[2:].rjust(8, '0')
 	if mode == "00000000":
 		c2_rezimas = False
 	elif mode == "11111111":
 		c2_rezimas = True
-	while True:
-		b = f.read(1)
-		if not b:
-			break
-		b = bin(ord(b))[2:].rjust(8, '0')
-		baitai.append(b)
-	
-	kodas_str = ""
-	for b in baitai:
-		kodas_str += b
-		
+
 	if c2_rezimas:
-		c2_decode(kodas_str)
+		#print ("aaaa")
+		c2_decode(uzkoduota, rezultatas)
 	else:
-		c1_decode(kodas_str)
-		
-current_location = 256
-all_bytes = []
-for k in values:
-	byte_index = current_location - k - 1
-	byte = locations[byte_index]
-	locations = {v: k for k, v in locations.items()}
-	locations[byte] = current_location
-	locations = {v: k for k, v in locations.items()}
-	all_bytes.append(byte)
-	current_location += 1
+		c1_decode(uzkoduota, rezultatas)
 
-allbytestr = ''.join(all_bytes)
-allbytes = [allbytestr[i:i+8] for i in range(0, len(allbytestr), 8)]
 
-with open(rezultatas, "wb") as wf:
-	for byte in allbytes:
-		n = int(byte, 2)
-		data = bytes([n])
-		wf.write(data)
+print(time.time() - start_time, " seconds.")
